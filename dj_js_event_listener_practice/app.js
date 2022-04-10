65741388b534f7e8004173765f0902bd752cd9b7
Melissa Weirick <melissaweirickdesigns@gmail.com>
2022-04-10 12:13:38 -0600

    I am uploading the start of the DJ DOM assignment for assistance in scroll event

---
+++ dj_js_event_listener_practice/app.js
@@ -0,0 +1,26 @@
+let djSquare = document.getElementById("square");
+djSquare.addEventListener("mouseover", blueSquare);
+djSquare.addEventListener("mousedown", redSquare);
+djSquare.addEventListener("mouseup", yellowSquare);
+djSquare.addEventListener("dblclick", greenSquare);
+window.addEventListener("scroll", orangeSquare, false);
+
+function blueSquare() {
+    document.append(djSquare.style.backgroundColor = 'blue')
+};
+
+function redSquare() {
+    document.append(djSquare.style.backgroundColor = 'red')
+};
+
+function yellowSquare() {
+    document.append(djSquare.style.backgroundColor = 'yellow')
+};
+
+function greenSquare() {
+    document.append(djSquare.style.backgroundColor = 'green')
+};
+
+function orangeSquare() {
+    document.append(djSquare.style.backgroundColor = 'orange')
+};
