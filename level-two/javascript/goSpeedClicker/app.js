7e7ee1cdd17f10c59fc682a22739afb72b1d74b2
Melissa Weirick <melissaweirickdesigns@gmail.com>
2022-07-06 02:03:38 -0600

    updating innerHTML to textContent

--- level-two/javascript/goSpeedClicker/app.js
+++ level-two/javascript/goSpeedClicker/app.js
@@ -4,13 +4,13 @@ const clickMe = document.getElementById("clickMe");
 let totalClicks = document.getElementById("totalClicks");
 
 function totalClicksDisplay() {
-    totalClicks.innerHTML = localStorage.getItem("totalClicksStorage");
+    totalClicks.textContent = localStorage.getItem("totalClicksStorage");
 };
 
 totalClicksDisplay();
 
 clickMe.addEventListener('click', (e) => {
-    totalClicksNewTotal = Math.floor(Number(totalClicks.innerHTML) + Number(1));
+    totalClicksNewTotal = Math.floor(Number(totalClicks.textContent) + Number(1));
     localStorage.setItem("totalClicksStorage", totalClicksNewTotal);
     totalClicksDisplay();
 });
