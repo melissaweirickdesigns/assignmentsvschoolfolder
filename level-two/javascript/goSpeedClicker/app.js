f5f32df13373e9e17a856cf9f1445e0bad0e2647
Melissa Weirick <melissaweirickdesigns@gmail.com>
2022-07-09 12:33:02 -0600

    adding the start button, countdown timer and reset functionality

--- level-two/javascript/goSpeedClicker/app.js
+++ level-two/javascript/goSpeedClicker/app.js
@@ -1,7 +1,12 @@
 // const readline = require("readline-sync");
 
 const clickMe = document.getElementById("clickMe");
+const reset = document.getElementById("reset");
+const start = document.getElementById("start");
+let count = document.getElementById("count");
 let totalClicks = document.getElementById("totalClicks");
+clickMe.disabled = true;
+let timerActivated = false;
 
 function totalClicksDisplay() {
     totalClicks.textContent = localStorage.getItem("totalClicksStorage");
@@ -9,9 +14,47 @@ function totalClicksDisplay() {
 
 totalClicksDisplay();
 
+function startTimer() {
+    let timerCountDown = 10;
+    setInterval(function() {
+    timerCountDown--;
+      if (timerCountDown >= 0) {
+        count.textContent = timerCountDown;
+      }
+      if (timerCountDown === 0) {
+          gameOver();
+      }
+    }, 1000);
+};
+
+function gameOver() {
+    clickMe.disabled = true;
+};
+
 clickMe.addEventListener('click', (e) => {
     totalClicksNewTotal = Math.floor(Number(totalClicks.textContent) + Number(1));
     localStorage.setItem("totalClicksStorage", totalClicksNewTotal);
     totalClicksDisplay();
 });
+
+reset.addEventListener('click', (e) => {
+    totalClicksNewTotal = "0";
+    localStorage.setItem("totalClicksStorage", totalClicksNewTotal);
+    totalClicksDisplay();
+    clickMe.disabled = true;
+    count.textContent = 10;
+    timerCountDown = 10;
+    start.disabled = false;
+    timerActivated = false;
+});
+
+
+start.addEventListener('click', (e) => {
+    clickMe.disabled = false;
+    start.disabled = true;
+    startTimer();
+});
+
+
+
 
