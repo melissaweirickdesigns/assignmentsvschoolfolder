e803d6b4b36170401f60d823bef7378e1871dd7b
Melissa Weirick <melissaweirickdesigns@gmail.com>
2022-07-09 18:40:48 -0600

    adding completed Array Filter Exercises

---
+++ level-two/javascript/arrayFilterExercises/app.js
@@ -0,0 +1,79 @@
+const readline = require("readline-sync");
+
+//1. Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
+
+function fiveAndGreaterOnly(arr) {
+    const greaterThanFive = arr.filter(moreThanFive => moreThanFive > 5);
+    return greaterThanFive;
+};
+
+console.log(fiveAndGreaterOnly([3, 6, 8, 2])); /// [6, 8]
+
+//2. Given an array of numbers, return a new array that only includes the even numbers.
+
+function evensOnly(arr) {
+    const even = arr.filter(evenOnly => evenOnly % 2 === 0);
+    return even;
+  }
+  // test
+  console.log(evensOnly([3, 6, 8, 2])); /// [6, 8, 2]
+
+//3. Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
+
+function fiveCharactersOrFewerOnly(arr) {
+    const wordsLessThanFive = arr.filter(word => word.length <= 5);
+    return wordsLessThanFive;
+  }
+  // test
+  console.log(fiveCharactersOrFewerOnly(["dog", "wolf", "by", "family", "eaten", "camping"])); // ["by", "dog", "wolf", "eaten"]
+  
+//4. Given an array of people objects, return a new array that has filtered out all those who don't belong to the club.
+
+function peopleWhoBelongToTheIlluminati(arr){
+    const members = arr.filter(memberList => memberList.member === true);
+    return members;
+  }
+  // test
+  console.log(peopleWhoBelongToTheIlluminati([
+      { name: "Angelina Jolie", member: true },
+      { name: "Eric Jones", member: false },
+      { name: "Paris Hilton", member: true },
+      { name: "Kayne West", member: false },
+      { name: "Bob Ziroll", member: true }
+  ]));
+  // =>
+  //[ { name: 'Angelina Jolie', member: true },
+  //  { name: 'Paris Hilton', member: true },
+  //  { name: 'Bob Ziroll', member: true } ]
+  
+//5. Make a filtered list of all the people who are old enough to see The Matrix (older than 18)
+
+function ofAge(arr){
+    const ofAge = arr.filter(oldEnough => oldEnough.age > 18);
+    return ofAge;
+  }
+  // test
+  console.log(ofAge([
+      { name: "Angelina Jolie", age: 80 },
+      { name: "Eric Jones", age: 2 },
+      { name: "Paris Hilton", age: 5 },
+      { name: "Kayne West", age: 16 },
+      { name: "Bob Ziroll", age: 100 }
+  ]));
+  // =>
+  //[ { name: 'Angelina Jolie', age: 80 },
+  //  { name: 'Bob Ziroll', age: 100 } ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
