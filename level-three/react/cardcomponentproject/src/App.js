dea502392329f4d03ed41ed8aacbcc66dd2642c9
Melissa Weirick <melissaweirickdesigns@gmail.com>
2023-03-08 03:08:45 -0700

    updating flexbox on card component

--- level-three/react/cardcomponentproject/src/App.js
+++ level-three/react/cardcomponentproject/src/App.js
@@ -11,30 +11,32 @@ function App() {
       <Header />
       <PhotoCollage />
       <OnlineExperiences />
-      <CardComponent 
-        profilePicture={require("./photos/profile-photo.png")}
-        reviewRating="5.0"
-        numberOfReviews="6"
-        country="USA"
-        descrip="Life lessons with Katie Zaferes"
-        pricePerPerson="$136"
-      />
-      <CardComponent 
-        profilePicture={require("./photos/wedding-photography-1.png")}
-        reviewRating="5.0"
-        numberOfReviews="30"
-        country="USA"
-        descrip="Learn Wedding Photography"
-        pricePerPerson="$125"
-      />
-      <CardComponent 
-        profilePicture={require("./photos/mountain-bike-1.png")}
-        reviewRating="4.8"
-        numberOfReviews="2"
-        country="USA"
-        descrip="Group Mountain Biking"
-        pricePerPerson="$50"
-      />
+      <div className="card--container">
+        <CardComponent 
+          profilePicture={require("./photos/profile-photo.png")}
+          reviewRating="5.0"
+          numberOfReviews="6"
+          country="USA"
+          descrip="Life lessons with Katie Zaferes"
+          pricePerPerson="$136"
+        />
+        <CardComponent 
+          profilePicture={require("./photos/wedding-photography-1.png")}
+          reviewRating="5.0"
+          numberOfReviews="30"
+          country="USA"
+          descrip="Learn Wedding Photography"
+          pricePerPerson="$125"
+        />
+        <CardComponent 
+          profilePicture={require("./photos/mountain-bike-1.png")}
+          reviewRating="4.8"
+          numberOfReviews="2"
+          country="USA"
+          descrip="Group Mountain Biking"
+          pricePerPerson="$50"
+        />
+      </div>  
     </div>
   );
 }
--- level-three/react/cardcomponentproject/src/components/CardComponent.js
+++ level-three/react/cardcomponentproject/src/components/CardComponent.js
@@ -2,20 +2,18 @@ import React from "react";
 
 export default function CardComponent(props) {
     return (
-       <div className="card--container"> 
-            <div class="card--row">
-                <div className="card">
-                    <img src={props.profilePicture} alt="profile photo" className="card--image"/>
-                    <div className="card--stats">
-                        <img src={require("../photos/star-rating.png")} alt="star rating photo" className="card--star margin side-margin"/>
-                        <span className="review-rating gray margin side-margin">{props.reviewRating}</span>
-                        <span className="number-of-reviews gray margin side-margin"><span>(</span>{props.numberOfReviews}<span>)</span></span>
-                        <span className="bullet-point margin side-margin">•</span>
-                        <span className="country margin side-margin">{props.country}</span>
-                    </div>
-                    <p className="description margin">{props.descrip}</p>
-                    <p className="price margin"><span className="bold">From <span className="price-amount">{props.pricePerPerson}</span></span> / person</p>
+       <div className="card-container-2"> 
+            <div className="card">
+                <img src={props.profilePicture} alt="profile photo" className="card--image"/>
+                <div className="card--stats">
+                    <img src={require("../photos/star-rating.png")} alt="star rating photo" className="card--star margin side-margin"/>
+                    <span className="review-rating gray margin side-margin">{props.reviewRating}</span>
+                    <span className="number-of-reviews gray margin side-margin"><span>(</span>{props.numberOfReviews}<span>)</span></span>
+                    <span className="bullet-point margin side-margin">•</span>
+                    <span className="country margin side-margin">{props.country}</span>
                 </div>
+                <p className="description margin">{props.descrip}</p>
+                <p className="price margin"><span className="bold">From <span className="price-amount">{props.pricePerPerson}</span></span> / person</p>
             </div>
         </div>
     )
