ee59f4c2e0e7d8b4422a667eebd2657ec38d848c
Melissa Weirick <melissaweirickdesigns@gmail.com>
2023-07-11 00:14:52 -0600

    adding my updated first express server

--- level-five/first-react-server/server.js
+++ level-five/first-react-server/server.js
@@ -1,10 +1,26 @@
 const express = require("express")
 const app = express()
 
-app.get("/registration", (req, res) => {
-    res.send({name: "Joe", age:"26"})
+app.use(express.json())
+
+const users = [
+    {name: "joe", age: "5"},
+    {name: "Moe", age: "6"},
+    {name: "Shmoe", age: "7"},
+    {name: "Jill", age: "8"},
+    {name: "Bill", age: "9"},
+]
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+
+app.post("/users", (req, res) => {
+    const newUser = req.body
+    users.push(newUser)
+    res.send(`Successfully added ${newUser.name} as a user`)
 })
 
 app.listen(9000, () => {
-    console.log("The Registration was submitted")
+    console.log("Registration Submitted")
 })
